f928cc5d81aa5f142521e2f767fc85a6b48399f7
Anton Shevchenko <antonshevchenko@icloud.com>
2015-09-19 14:16:50 -0400

    Gucci error handling

--- mobile/www/js/findbluetooth.js
+++ mobile/www/js/findbluetooth.js
@@ -1,48 +1,72 @@
-app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $interval, $scope) {
+app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $ionicLoading, $interval, $scope) {
 
     $ionicPlatform.ready(function() {
 
         $scope.golfBalls = [];
+        $scope.golfBall = null;
 
-        ble.startScan([], function(device) {
-            if (device.name != undefined && device.name.substring(0, 4) == 'GOLF') {
-              var isNewGolfBall = true;
-              for (var i = 0; i < $scope.golfBalls.length; i++) {
-                if (device.name == $scope.golfBalls[i].name) {
-                  isNewGolfBall = false;
+        function scanForGolfBalls() {
+            ble.startScan([], function(device) {
+                console.log('New device: ' + device);
+                if (device.name != undefined && device.name.substring(0, 4) == 'GOLF') {
+                    var isNewGolfBall = true;
+                    for (var i = 0; i < $scope.golfBalls.length; i++) {
+                        if (device.name == $scope.golfBalls[i].name) {
+                            isNewGolfBall = false;
+                        }
+                    }
+                    if (isNewGolfBall) {
+                        $scope.golfBalls.push(device);
+                        showPopup();
+                    }
                 }
-              }
-              if (isNewGolfBall) {
-                  $scope.golfBalls.push(device);
-                  showPopup();
-              }
-            }
-        });
+            });
+        }
 
         function showPopup() {
-          if ($scope.popup) {
-            $scope.popup.close();
-          }
-          $scope.popup = $ionicPopup.show({
-            template: 'Connect to a Golf ball:',
-            scope: $scope,
-            buttons: $scope.golfBalls.map(function(golfBall) {
-              return {
-                text: golfBall.name,
-                onTap: function() {
-                  connectToGolfBall(golfBall);
-                }
-              };
-            })
-          });
+            if ($scope.popup) {
+                $scope.popup.close();
+            }
+            $scope.popup = $ionicPopup.show({
+                template: 'Connect to a Golf ball:',
+                scope: $scope,
+                buttons: $scope.golfBalls.map(function(golfBall) {
+                    return {
+                        text: golfBall.name,
+                        onTap: function() {
+                          connectToGolfBall(golfBall);
+                        }
+                    };
+                })
+            });
         }
 
         function connectToGolfBall(golfBall) {
-          ble.stopScan();
-          ble.connect(golfBall.id, function(success) {
-            console.log('Connected to ball!');
-          });
+            ble.stopScan();
+            $ionicLoading.show({
+                template: '<ion-spinner></ion-spinner><p class="text-no-margin">Connecting to golf ball...</p>'
+            });
+            ble.connect(golfBall.id, function(success) {
+                $scope.golfBall = golfBall;
+                $ionicLoading.hide();
+                $ionicLoading.show({
+                    template: '<i class="icon ion-android-checkmark-circle"><p class="text-no-margin">Connected!</p>',
+                    duration: 1500
+                });
+            }, function(error) {
+                $ionicLoading.hide();
+                var errorPopup = $ionicPopup.alert({
+                    title: 'Connection Error',
+                    template: 'Can\'t connect to the golf ball.'
+                });
+                errorPopup.then(function() {
+                    scanForGolfBalls();
+                });
+            });
         }
+
+        // Scan for golf balls from the start
+        scanForGolfBalls();
 
     });
 

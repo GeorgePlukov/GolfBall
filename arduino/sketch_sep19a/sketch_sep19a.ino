ff16f00fb103e2f591816056af96527dc8b89090
GeorgePlukov <gplukov@gmail.com>
2015-09-19 15:10:54 -0400

    Changed to blue bean

--- arduino/sketch_sep19a/sketch_sep19a.ino
+++ arduino/sketch_sep19a/sketch_sep19a.ino
@@ -2,27 +2,29 @@ int sensorValue;
 int sensorLow = 1023;
 int sensorHigh = 0;
 int previousLight;
-const int ledPin = 8;
+const int sensorPowerPin = 5;
 
 void setup() {
-  Serial.begin(9600);
   
+  Serial.begin(57600);
+  Bean.setLed(100,100,255);
+
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  pinMode(sensorPowerPin, OUTPUT);
+  digitalWrite(sensorPowerPin, HIGH);
 
   while (millis() < 2000){
     sensorValue = analogRead(A0);
-    
+
     if (sensorValue > sensorHigh){
       sensorHigh = sensorValue;
+      
     }
     if (sensorValue < sensorLow){
       sensorLow = sensorValue;
     }
   }
   previousLight = sensorValue;
-  digitalWrite(ledPin, LOW);
   
 } 
 
@@ -33,16 +35,15 @@ void loop() {
   Serial.println(sensorValue);
   // TUrn the LED on when the change in sensor value goes down a large amount quickly
   if ((previousLight - sensorValue) > 50){
-    Serial.println("LED ON: " +  String(previousLight - sensorValue, DEC));
-    digitalWrite(ledPin, HIGH);
+    Serial.println("SCORE: " +  String(previousLight - sensorValue, DEC));
+    
   }
   // Turn the led off when the sensor goes up a large amount
-  if ((sensorValue - previousLight) > 50){
-    Serial.println("LED OFF: " + String(sensorValue - previousLight, DEC));
-    digitalWrite(ledPin, LOW);
+  if ((sensorValue - previousLight) > 25){
+    Serial.println("UNSCORE: " + String(sensorValue - previousLight, DEC));
   }
   previousLight = sensorValue;
-  delay(750);
+  delay(500);
 }
 
 

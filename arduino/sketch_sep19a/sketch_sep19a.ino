8cdc2adc7bc4eb8cc27e53cd9d3b17e5e742f46e
GeorgePlukov <gplukov@gmail.com>
2015-09-19 19:37:06 -0400

    streamlined

--- arduino/sketch_sep19a/sketch_sep19a.ino
+++ arduino/sketch_sep19a/sketch_sep19a.ino
@@ -7,15 +7,15 @@ const int ledPin = 3;
 
 void setup() {
   
-  Serial.begin(57600);
-  Bean.setLed(255,0,0);
+  
+  Bean.setLed(255,200,0);
   Bean.enableConfigSave(false);
-  Bean.setBeanName("GOLF:false");
+  Bean.setBeanName("HOLE:false");
   // put your setup code here, to run once:
   pinMode(sensorPowerPin, OUTPUT);
-  //pinMode(ledPin, OUTPUT);
+  
   digitalWrite(sensorPowerPin, HIGH);
- // digitalWrite(ledPin, HIGH);
+  
   while (millis() < 2000){
     sensorValue = analogRead(A0);
 
@@ -35,23 +35,22 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
+  //Serial.println(sensorValue);
   // TUrn the LED on when the change in sensor value goes down a large amount quickly
   if ((previousLight - sensorValue) > 50){
-    Serial.println("SCORE: " +  String(previousLight - sensorValue, DEC));
+    //Serial.println("SCORE: " +  String(previousLight - sensorValue, DEC));
     String message = "true";
     Bean.setBeanName("HOLE:" + message);
-    Bean.setLed(0,255,0);
+
   }
   // Turn the led off when the sensor goes up a large amount
   if ((sensorValue - previousLight) > 25){
-    Serial.println("UNSCORE: " + String(sensorValue - previousLight, DEC));
+    //Serial.println("UNSCORE: " + String(sensorValue - previousLight, DEC));
     String message = "false";
     Bean.setBeanName("HOLE:" + message);
-    Bean.setLed(255,0,0);
   }
   previousLight = sensorValue;
-  delay(500);
+  delay(700);
 }
 
 

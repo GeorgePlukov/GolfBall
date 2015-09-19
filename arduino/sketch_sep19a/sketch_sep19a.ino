47c0f6bca375df86dd58706d5670b4858b29f46d
GeorgePlukov <gplukov@gmail.com>
2015-09-19 02:53:41 -0400

    Printing light sensor reading

--- arduino/sketch_sep19a/sketch_sep19a.ino
+++ arduino/sketch_sep19a/sketch_sep19a.ino
@@ -2,12 +2,42 @@ int sensorValue;
 int sensorLow = 1023;
 int sensorHigh = 0;
 
-void setup() {
-  // put your setup code here, to run once:
+const int ledPin = 8;
 
-}
+void setup() {
+  Serial.begin(9600);
+  
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  while (millis() < 5000){
+    sensorValue = analogRead(A0);
+    
+    if (sensorValue > sensorHigh){
+      sensorHigh = sensorValue;
+    }
+    if (sensorValue < sensorLow){
+      sensorLow = sensorValue;
+    }
+  }
+  digitalWrite(ledPin, LOW);
+  
+} 
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  if (sensorValue > 0){
+    digitalWrite(ledPin, HIGH);
+  }
 }
+
+
+
+
+
+
+
+

cf64d45ce0ba13c15d946ef46f375d3a446ed4cd
Anton Shevchenko <antonshevchenko@icloud.com>
2015-09-19 13:44:36 -0400

    Simplified finding new golf balls

--- mobile/www/js/findbluetooth.js
+++ mobile/www/js/findbluetooth.js
@@ -5,27 +5,17 @@ app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $interval,
         $scope.golfBalls = [];
 
         ble.startScan([], function(device) {
-            // console.log(device.name.substring(0,4) || 'undefined');
-            if (device.name == undefined) {
-
-            } else {
-                if (device.name.substring(0, 4) == 'GOLF') {
-                    if ($scope.golfBalls.length == 0) {
-                        $scope.golfBalls.push(device);
-                        showPopup();
-                    } else {
-                        for (var i = 0; i < $scope.golfBalls.length; i++) {
-                            if (device.name == $scope.golfBalls[i].name) {
-                                break;
-                            }
-                            if (i == $scope.golfBalls.length - 1) {
-                                $scope.golfBalls.push(device);
-                                showPopup();
-                            }
-                        }
-                    }
-                    console.log($scope.golfBalls);
+            if (device.name != undefined && device.name.substring(0, 4) == 'GOLF') {
+              var isNewGolfBall = true;
+              for (var i = 0; i < $scope.golfBalls.length; i++) {
+                if (device.name == $scope.golfBalls[i].name) {
+                  isNewGolfBall = false;
                 }
+              }
+              if (isNewGolfBall) {
+                  $scope.golfBalls.push(device);
+                  showPopup();
+              }
             }
         });
 

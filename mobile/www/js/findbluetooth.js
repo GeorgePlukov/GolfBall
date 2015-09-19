fc7b0ecf98b4718d944429e0195326dcfcdf70de
andrewmcewen <andrew.mcewen96@gmail.com>
2015-09-19 14:06:29 -0400

    added scorecard

--- mobile/www/js/findbluetooth.js
+++ mobile/www/js/findbluetooth.js
@@ -3,58 +3,72 @@ app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $interval,
     $ionicPlatform.ready(function() {
 
         $scope.golfBalls = [];
-        $scope.holes = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        $scope.holes1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        $scope.holes2 = ['10', '11', '12', '13', '14', '15', '16', '17', '18'];
+        $scope.pars = ['4', '4', '5', '3', '4', '3', '4', '4', '5'];
+        $scope.scores = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
+        $scope.frontNine = true;
 
-        ble.startScan([], function(device) {
-            // console.log(device.name.substring(0,4) || 'undefined');
-            if (device.name == undefined) {
+        if (window.cordova) {
 
-            } else {
-                if (device.name.substring(0, 4) == 'GOLF') {
-                    if ($scope.golfBalls.length == 0) {
-                        $scope.golfBalls.push(device);
-                        showPopup();
-                    } else {
-                        for (var i = 0; i < $scope.golfBalls.length; i++) {
-                            if (device.name == $scope.golfBalls[i].name) {
-                                break;
-                            }
-                            if (i == $scope.golfBalls.length - 1) {
-                                $scope.golfBalls.push(device);
-                                showPopup();
+            ble.startScan([], function(device) {
+                // console.log(device.name.substring(0,4) || 'undefined');
+                if (device.name == undefined) {
+
+                } else {
+                    if (device.name.substring(0, 4) == 'GOLF') {
+                        if ($scope.golfBalls.length == 0) {
+                            $scope.golfBalls.push(device);
+                            showPopup();
+                        } else {
+                            for (var i = 0; i < $scope.golfBalls.length; i++) {
+                                if (device.name == $scope.golfBalls[i].name) {
+                                    break;
+                                }
+                                if (i == $scope.golfBalls.length - 1) {
+                                    $scope.golfBalls.push(device);
+                                    showPopup();
+                                }
                             }
                         }
+                        console.log($scope.golfBalls);
                     }
-                    console.log($scope.golfBalls);
                 }
-            }
-        });
+            });
+        }
 
         function showPopup() {
-          if ($scope.popup) {
-            $scope.popup.close();
-          }
-          $scope.popup = $ionicPopup.show({
-            template: 'Connect to a Golf ball:',
-            scope: $scope,
-            buttons: $scope.golfBalls.map(function(golfBall) {
-              return {
-                text: golfBall.name,
-                onTap: function() {
-                  connectToGolfBall(golfBall);
-                }
-              };
-            })
-          });
+            if ($scope.popup) {
+                $scope.popup.close();
+            }
+            $scope.popup = $ionicPopup.show({
+                template: 'Connect to a Golf ball:',
+                scope: $scope,
+                buttons: $scope.golfBalls.map(function(golfBall) {
+                    return {
+                        text: golfBall.name,
+                        onTap: function() {
+                            connectToGolfBall(golfBall);
+                        }
+                    };
+                })
+            });
         }
 
         function connectToGolfBall(golfBall) {
-          ble.stopScan();
-          ble.connect(golfBall.id, function(success) {
-            console.log('Connected to ball!');
-          });
+            ble.stopScan();
+            ble.connect(golfBall.id, function(success) {
+                console.log('Connected to ball!');
+            });
         }
 
+        $scope.changeNine = function() {
+            $scope.frontNine = !$scope.frontNine;
+        }
+
+
     });
+
+
 
 });

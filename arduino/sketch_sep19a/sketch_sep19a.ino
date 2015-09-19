cfafde441fe126888ce88caf6666b7b8693f7b1a
GeorgePlukov <gplukov@gmail.com>
2015-09-19 09:50:15 -0400

    Working light sensor based on previous readings

--- arduino/sketch_sep19a/sketch_sep19a.ino
+++ arduino/sketch_sep19a/sketch_sep19a.ino
@@ -1,7 +1,7 @@
 int sensorValue;
 int sensorLow = 1023;
 int sensorHigh = 0;
-
+int previousLight;
 const int ledPin = 8;
 
 void setup() {
@@ -11,7 +11,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
 
-  while (millis() < 5000){
+  while (millis() < 2000){
     sensorValue = analogRead(A0);
     
     if (sensorValue > sensorHigh){
@@ -21,17 +21,28 @@ void setup() {
       sensorLow = sensorValue;
     }
   }
+  previousLight = sensorValue;
   digitalWrite(ledPin, LOW);
   
 } 
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensorValue = analogRead(A0);
   Serial.println(sensorValue);
-  if (sensorValue > 0){
+  // TUrn the LED on when the change in sensor value goes down a large amount quickly
+  if ((previousLight - sensorValue) > 50){
+    Serial.println("LED ON: " +  String(previousLight - sensorValue, DEC));
     digitalWrite(ledPin, HIGH);
   }
+  // Turn the led off when the sensor goes up a large amount
+  if ((sensorValue - previousLight) > 50){
+    Serial.println("LED OFF: " + String(sensorValue - previousLight, DEC));
+    digitalWrite(ledPin, LOW);
+  }
+  previousLight = sensorValue;
+  delay(750);
 }
 
 

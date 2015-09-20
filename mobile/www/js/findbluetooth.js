31b81607f2ae2ab567c61823032907e829cf4509
andrewmcewen <andrew.mcewen96@gmail.com>
2015-09-19 22:08:39 -0400

    gucci mane

--- mobile/www/js/findbluetooth.js
+++ mobile/www/js/findbluetooth.js
@@ -1,71 +1,144 @@
-app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $ionicLoading, $interval, $scope) {
+app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $ionicLoading, $interval, $scope, $timeout) {
 
     $ionicPlatform.ready(function() {
+
+
+
+
         var firstTimeConnecting = true;
         $scope.golfBalls = [];
         $scope.golfBall = null;
         $scope.popup = null;
         $scope.hole = null;
         $scope.score = 'n/a';
-        $scope.holes = [
-            { hole: 1, par: 4, score: null },
-            { hole: 2, par: 4, score: null },
-            { hole: 3, par: 5, score: null },
-            { hole: 4, par: 3, score: null },
-            { hole: 5, par: 4, score: null },
-            { hole: 6, par: 3, score: null },
-            { hole: 7, par: 4, score: null },
-            { hole: 8, par: 4, score: null },
-            { hole: 9, par: 5, score: null },
-            { hole: 10, par: 4, score: null },
-            { hole: 11, par: 5, score: null },
-            { hole: 12, par: 5, score: null },
-            { hole: 13, par: 3, score: null },
-            { hole: 14, par: 4, score: null },
-            { hole: 15, par: 4, score: null },
-            { hole: 16, par: 5, score: null },
-            { hole: 17, par: 5, score: null },
-            { hole: 18, par: 4, score: null },
-        ];
+        $scope.holes = [{
+            hole: 1,
+            par: 4,
+            score: null
+        }, {
+            hole: 2,
+            par: 4,
+            score: null
+        }, {
+            hole: 3,
+            par: 5,
+            score: null
+        }, {
+            hole: 4,
+            par: 3,
+            score: null
+        }, {
+            hole: 5,
+            par: 4,
+            score: null
+        }, {
+            hole: 6,
+            par: 3,
+            score: null
+        }, {
+            hole: 7,
+            par: 4,
+            score: null
+        }, {
+            hole: 8,
+            par: 4,
+            score: null
+        }, {
+            hole: 9,
+            par: 5,
+            score: null
+        }, {
+            hole: 10,
+            par: 4,
+            score: null
+        }, {
+            hole: 11,
+            par: 5,
+            score: null
+        }, {
+            hole: 12,
+            par: 5,
+            score: null
+        }, {
+            hole: 13,
+            par: 3,
+            score: null
+        }, {
+            hole: 14,
+            par: 4,
+            score: null
+        }, {
+            hole: 15,
+            par: 4,
+            score: null
+        }, {
+            hole: 16,
+            par: 5,
+            score: null
+        }, {
+            hole: 17,
+            par: 5,
+            score: null
+        }, {
+            hole: 18,
+            par: 4,
+            score: null
+        }, ];
         $scope.stats = {
-          first: '31 Par',
-          second: '12 Birdie',
-          third: '6 Bogey'
+            first: '31 Par',
+            second: '12 Birdie',
+            third: '6 Bogey'
         };
 
-        function scanForGolfBalls() {
-            if(window.ble === undefined) return;
 
-            ble.startScan([], function(device) {
-                console.log('New device: ' + device);
-                if (device.name != undefined) {
+        
+        $scope.searchUnpaired = function(){
 
-                  if ($scope.golfBall != null) {
-                      if (device.name == 'HOLE:true') {
-                          connectToGolfball($scope.golfBall);
-                      }
+            // bluetoothSerial.discoverUnpaired(function(data) {
+            //     $scope.searchUnpaired();
+            // }, function(err){
+            //     console.log("error", err);
+            // })
 
-                      if (device.id == $scope.golfBall.id) {
-                          updateScore();
-                      }
-                  } else {
-                      if (device.name.substring(0, 4) == 'GOLF') {
-                          var isNewGolfBall = true;
-                          for (var i = 0; i < $scope.golfBalls.length; i++) {
-                              if (device.name == $scope.golfBalls[i].name) {
-                                  isNewGolfBall = false;
-                              }
-                          }
-                          if (isNewGolfBall) {
-                              $scope.golfBalls.push(device);
-                              showPopup();
-                          }
-                      }
-                  }
-              }
+            // bluetoothSerial.setDeviceDiscoveredListener(function(device) {
+            //      if (device.name != undefined) {
 
-            });
+            //         if(device.name.substring(0,4) == 'GOLF'){
+            //             updateScore(device);
+            //         }
+
+            //     }
+            // });
+
+            $interval(function(){
+              ble.startScan([], function(device){
+                     if (device.name != undefined) {
+
+                        if(device.name.substring(0,4) == 'GOLF'){
+                            updateScore(device);
+                               ble.stopScan(
+                                    function() { console.log("Scan complete"); },
+                                    function() { console.log("stopScan failed"); }
+                                );
+                        }
+
+
+
+                    }
+                }, function(){
+
+                });
+
+          
+
+            }, 500)
+  
         }
+
+
+        $scope.searchUnpaired();
+
+
 
         function showPopup() {
             if ($scope.popup) {
@@ -87,11 +160,15 @@ app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $ionicLoad
             });
         }
 
-        function updateScore() {
-            var score = $scope.golfBall.name;
+        function updateScore(device) {
+            var score = device.name;
             score = score.substring(score.indexOf(':') + 1, score.indexOf(','));
             $scope.score = score;
-            $scope.holes[$scope.hole]['score'] = $scope.score;
+            console.log(device);
+            $timeout(function() {
+                $scope.$digest();
+            });
+           // $scope.holes[$scope.hole]['score'] = $scope.score;
         }
 
         function connectToGolfBall(golfBall) {
@@ -119,13 +196,11 @@ app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $ionicLoad
                     });
                 }
                 firstTimeConnecting = false;
-                
-                scanForHoles();
 
                 ble.disconnect(golfBall.id, function(success) {
-                  console.log('Disconnect ball successful');
+                    console.log('Disconnect ball successful');
                 }, function(error) {
-                  console.log('ERROR: Disconnect ball');
+                    console.log('ERROR: Disconnect ball');
                 });
             }, function(error) {
                 $ionicLoading.hide();
@@ -135,15 +210,15 @@ app.controller('bluetoothCtrl', function($ionicPlatform, $ionicPopup, $ionicLoad
                         template: 'Can\'t connect to the golf ball.'
                     });
                     errorPopup.then(function() {
-                        scanForGolfBalls();
+                        //scanForGolfBalls();
                     });
                 }
                 firstTimeConnecting = false;
             });
         }
-        
+
         // Scan for golf balls from the start
-        scanForGolfBalls();
+        // scanForGolfBalls();
 
     });
 
